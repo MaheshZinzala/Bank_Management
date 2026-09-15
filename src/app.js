2b64bdc0453b33ad02b0ee9608d9af8b2245dabc
MaheshZinzala <mbzinzala165@gmail.com>
2026-09-15 18:59:27 +0530

    Project setup

---
+++ src/app.js
@@ -0,0 +1,10 @@
+import express from "express";
+import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cookieParser());
+
+export default app;
